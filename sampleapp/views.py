201b04a11af0d770af69c5e85a32c5a1df51c1e9
George Zeng <georgejzeng@gmail.com>
2018-05-18 17:14:39 -0400

    Add routes for upload and download

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -3,3 +3,11 @@ from sampleapp import myapp
 @myapp.route('/')
 def helloworld():
     return 'Hello World!'
+
+@myapp.route('/upload', methods=['GET', 'POST'])
+def upload():
+    pass
+
+@myapp.route('/download', methods=['GET'])
+def download():
+    pass
