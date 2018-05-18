76ed9a03407db1b084b6c30f4871571f0fda1ead
George Zeng <georgejzeng@gmail.com>
2018-05-18 17:24:10 -0400

    Implement upload.html view using base.html

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,12 +1,14 @@
 from sampleapp import myapp
 
+from flask import render_template
+
 @myapp.route('/')
 def helloworld():
     return 'Hello World!'
 
 @myapp.route('/upload', methods=['GET', 'POST'])
 def upload():
-    pass
+    return render_template('upload.html')
 
 @myapp.route('/download', methods=['GET'])
 def download():
